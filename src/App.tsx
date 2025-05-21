af3c4286b579a9399e5278679cc4801ec49986be
Marinos.Sake <protutor13@hotmail.com>
2025-05-22 02:28:09 +0300

    feat: using states in components

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,32 @@
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFuctionalComponentWithProps.tsx";
-import ArrowFunctionalComponentWithPropsType from "./components/ArrowFuctionalComponentWithPropsType.tsx";
+// import ArrowFunctionalComponentWithPropsType from "./components/ArrowFuctionalComponentWithPropsType.tsx";
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFuctionalComponentWithProps.tsx";
+import Layout from "./components/Layout.tsx";
+import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
+// import FunctionalComponent from "./components/FunctionalComponent.tsx";
+// import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
+// import {CodingFactoryLogo} from "./components/CodingFactoryLogo.tsx";
 
 function App() {
 
   return (
     <>
+        <Layout>
         {/*<ClassComponent></ClassComponent>*/}
         {/*<FunctionalComponent></FunctionalComponent>*/}
         {/*<ArrowFunctionalComponent/>*/}
         {/*<ArrowFunctionalComponentWithProps></ArrowFunctionalComponentWithProps>*/}
-        <ArrowFunctionalComponentWithPropsType title="Is an Arrow Functional Component with Props!"
-                                               description="this is a description"/>
+        {/*<ArrowFunctionalComponentWithPropsType title="Is an Arrow Functional Component with Props!"*/}
+        {/*                                       description="this is a description"/>*/}
+        {/*<CodingFactoryLogo></CodingFactoryLogo>*/}
+
+            {/*<ClassComponentWithState></ClassComponentWithState>*/}
+
+            <FunctionalComponentWithState></FunctionalComponentWithState>
+
+        </Layout>
     </>
   )
 }

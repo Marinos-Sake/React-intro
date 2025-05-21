3ab1d986500db8beeb41dc80522a107cd9926e8e
Marinos.Sake <protutor13@hotmail.com>
2025-05-22 02:27:13 +0300

    feat: add logo

---
+++ src/components/CodingFactoryLogo.tsx
@@ -0,0 +1,14 @@
+const CodingFactoryLogo = () => {
+    return(
+        <>
+            <img
+                className="my-4 h-16"
+                src="/logocf.png"
+                alt="CF7"
+            />
+        </>
+    )
+}
+
+
+export { CodingFactoryLogo };

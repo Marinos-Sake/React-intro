fda4c33e2357dea62a7a8f2681ae5cb0ac310916
Marinos.Sake <protutor13@hotmail.com>
2025-05-29 20:49:16 +0300

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 // import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 import Layout from "./components/Layout.tsx";
+import Todo from "./components/Todo/Todo.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
@@ -12,7 +13,7 @@ import Layout from "./components/Layout.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import {CounterWithCustomHook} from "./components/CounterWithCustomHook.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
-import CounterWithReducer from "./components/CounterWithReducer.tsx";
+// import CounterWithReducer from "./components/CounterWithReducer.tsx";
 
 function App() {
 
@@ -36,7 +37,8 @@ function App() {
                 {/*<CounterAdvanced/>*/}
                 {/*<CounterWithCustomHook/>*/}
                 {/*<CounterAdvancedWithCustomHook/>*/}
-                <CounterWithReducer/>
+                {/*<CounterWithReducer/>*/}
+                <Todo/>
 
 
             </Layout>

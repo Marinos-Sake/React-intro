ad1704d58e458fd52d0956559dc57a7fad0b3b38
Marinos.Sake <protutor13@hotmail.com>
2025-06-10 19:08:23 +0300

    .

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -2,9 +2,11 @@ import {useEffect} from "react";
 import {useParams} from "react-router";
 
 const UserPage = () => {
+
     const {userId} = useParams();
+
     useEffect(() => {
-        document.title = `CF7 User id: ${userId}`;
+        document.title = `CF7 User id: ${ userId }`;
     }, [userId]);
 
     return (
